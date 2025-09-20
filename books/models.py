54d8b54db06ac8c854ebcfbff68e3951342113dc
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-20 16:32:12 +0300

    refactor: changed __str__ method and CoverChoices

--- books/models.py
+++ books/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 class Book(models.Model):
     class CoverChoices(models.TextChoices):
-        HARD = "hard", "Hard"
-        SOFT = "soft", "Soft"
+        HARD = "HARD", "Hard"
+        SOFT = "SOFT", "Soft"
 
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
@@ -23,5 +23,4 @@ class Book(models.Model):
         ordering = ["title"]
 
     def __str__(self):
-        return (f"{self.title} - Cover: {self.get_cover_display()}, "
-                f"Daily fee: ${self.daily_fee}")
+        return self.title
