fe49547effaade140a2ea29ac0eaccf493d8485a
Bohdan Yagginov <bhders62@gmail.com>
2025-09-20 16:07:10 +0300

    feature: added user model with email insted username

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.managers import UserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField("email address", unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
