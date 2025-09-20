1fe1477e47ec676999fdb6f3c3161a93ecf718d6
Bohdan Yagginov <bhders62@gmail.com>
2025-09-20 19:50:13 +0300

    tests: added tests for UserRegistrationView

--- users/tests/test_user_api.py
+++ users/tests/test_user_api.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
 
 from users.serializers import UserRegistrationSerializer
@@ -36,3 +38,38 @@ class TestUserRegistrationSerializer(APITestCase):
         self.user_data["email"] = "not email at all"
         serializer = UserRegistrationSerializer(data=self.user_data)
         self.assertFalse(serializer.is_valid())
+
+
+class TestUserRegistrationViewSet(APITestCase):
+    def setUp(self):
+        self.user_data = {
+            "email": "test@test.com",
+            "password1": "(*FDfas9hdgoaisdoi(&f0du",
+            "password2": "(*FDfas9hdgoaisdoi(&f0du",
+        }
+        self.url = reverse("users:register")
+
+    def test_create_user_with_valid_data(self):
+        response = self.client.post(self.url, data=self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(email=self.user_data["email"]).exists())
+
+    def test_create_second_user_with_same_email(self):
+        User.objects.create_user(email="test@test.com", password="p")
+
+        response = self.client.post(self.url, data=self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
+
+    def test_create_user_with_wrong_email(self):
+        self.user_data["email"] = "not email at all"
+        response = self.client.post(self.url, self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
+
+    def test_create_user_with_not_mathed_password(self):
+        self.user_data["password2"] = "not the same"
+        response = self.client.post(self.url, self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("non_field_errors", response.data)
+        self.assertTrue(any("passwords" in error.lower() for error in response.data["non_field_errors"]))
