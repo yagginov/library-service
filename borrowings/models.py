ecc7ad36a2bfe676c1c17e1585d8bfb7368993d6
Farzam <farzamgt@gmail.com>
2025-09-20 17:37:48 +0300

    added Borrowing model with relations to User and Book

--- borrowings/models.py
+++ borrowings/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
+from books.models import Book
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    borrow_date = models.DateField()
+    expected_return_date = models.DateField()
+    actual_return_date = models.DateField(null=True, blank=True)
+
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="borrowings"
+    )
+
+    class Meta:
+        ordering = ["-borrow_date"]
+
+    def __str__(self):
+        return (f"{self.user} borrowed "
+                f"{self.book} on {self.borrow_date}")
