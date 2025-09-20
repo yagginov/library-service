cd3e6da1a85d140b6d9d726ccbcebebad496e233
Farzam <farzamgt@gmail.com>
2025-09-20 18:12:32 +0300

    fix(models): set borrow_date auto_now_add and optimize __str__ to use IDs

--- borrowings/models.py
+++ borrowings/models.py
@@ -5,24 +5,24 @@ from books.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField()
+    borrow_date = models.DateField(auto_now_add=True)
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(null=True, blank=True)
 
     book = models.ForeignKey(
         Book,
         on_delete=models.CASCADE,
-        related_name="borrowings"
+        related_name="borrowings",
     )
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
-        related_name="borrowings"
+        related_name="borrowings",
     )
 
     class Meta:
         ordering = ["-borrow_date"]
 
     def __str__(self):
-        return (f"{self.user} borrowed "
-                f"{self.book} on {self.borrow_date}")
+        return (f"{self.user_id} borrowed "
+                f"{self.book_id} on {self.borrow_date}")
