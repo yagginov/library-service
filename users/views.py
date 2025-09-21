dc05fb5883c0828cccfcb7a5c8d9d708612304e1
Otstoy <otstoy.sxe@gmail.com>
2025-09-21 12:38:45 +0300

    feat(user): add me endpoint for retrieving and updating current user profile

--- users/views.py
+++ users/views.py
@@ -1,7 +1,17 @@
+from rest_framework import generics
 from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-from users.serializers import UserRegistrationSerializer
+from users.serializers import UserProfileSerializer, UserRegistrationSerializer
 
 
 class UserRegistrationView(CreateAPIView):
     serializer_class = UserRegistrationSerializer
+
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
