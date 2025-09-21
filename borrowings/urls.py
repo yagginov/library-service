7051abd39a93f17c4832676131fc4d8dee432184
Farzam <farzamgt@gmail.com>
2025-09-21 09:24:41 +0300

    feat(borrowings): add GET /borrowings/ with user_id and is_active filters

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from .views import BorrowingCreateView
+from .views import BorrowingCreateView, BorrowingDetailView, BorrowingListView
 
 app_name = "borrowings"
 
 urlpatterns = [
     path("", BorrowingCreateView.as_view(), name="borrowings-create"),
+    path("list/", BorrowingListView.as_view(), name="borrowings-list"),
+    path("<int:pk>/", BorrowingDetailView.as_view(), name="borrowings-detail"),
 ]
