addd5f2c22bed064ae1a51ba12f8367f67b222e0
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-21 21:19:32 +0300

    feature: create new permission for borrowing view

--- base/permissions.py
+++ base/permissions.py
@@ -24,3 +24,19 @@ class IsAdminOrObjectOwner(BasePermission):
         if request.user.is_staff:
             return True
         return obj.user == request.user
+
+
+class IsAdminOrOwnerWithCreatePermission(BasePermission):
+    def has_permission(self, request, view):
+        if not (request.user and request.user.is_authenticated):
+            return False
+
+        if request.user.is_staff:
+            return True
+
+        return request.method in ["GET", "POST"]
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.user == request.user
