4a0a0cfe4a79ffa7c27c39389c85a0e8342407fa
Otstoy <otstoy.sxe@gmail.com>
2025-09-21 17:11:39 +0300

    feat(notifications): add notification interface

---
+++ notifications/services.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+from telegram import Bot
+
+from config import settings
+from notifications.interfaces import NotificationInterface
+
+load_dotenv()
+
+
+class NotificationTelegramService(NotificationInterface):
+
+    def __init__(self, bot_token: str = settings.TG_API_TOKEN, admin_chat_id: int = settings.CHAT_ID):
+        self.bot = Bot(token=bot_token)
+        self.admin_chat_id = admin_chat_id
+
+    def send(self, message: str) -> None:
+        self.bot.send_message(chat_id=self.admin_chat_id, text=message)
+
+notification = NotificationTelegramService()
