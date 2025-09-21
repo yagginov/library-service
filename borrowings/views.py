e7e785833f5e57e2074b90fd739678b164d98450
Farzam <farzamgt@gmail.com>
2025-09-21 08:48:29 +0300

    feat(borrowings): implement borrowing creation with inventory update

--- borrowings/views.py
+++ borrowings/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from .models import Borrowing
+from .serializers import BorrowingCreateSerializer
+
+
+class BorrowingCreateView(generics.CreateAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingCreateSerializer
+    permission_classes = [permissions.IsAuthenticated]
