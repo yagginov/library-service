2f1aeb94b31977d48860b8dbf3879ccdc2c6e32b
Farzam <farzamgt@gmail.com>
2025-09-21 13:55:48 +0300

    chore(borrowings): clean up imports in views.py and use BorrowingFilter

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import BorrowingViewSet
+from borrowings.views import BorrowingViewSet
 
 app_name = "borrowings"
 
