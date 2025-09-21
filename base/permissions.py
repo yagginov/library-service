995e92bcf19e2eaeea35a636dfcd68abc2ff917c
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-21 15:43:58 +0300

    feature: created two custom permissions IsAdminOrIsAuthenticatedReadOnly and IsAdminOrObjectOwner

---
+++ base/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework.permissions import SAFE_METHODS, BasePermission
+
+
+class IsAdminOrIsAuthenticatedReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            (
+                request.method in SAFE_METHODS
+                and request.user
+                and request.user.is_authenticated
+            )
+            or (request.user and request.user.is_staff)
+        )
+
+
+class IsAdminOrObjectOwner(BasePermission):
+    def has_permission(self, request, view):
+        return bool(
+            request.user
+            and request.user.is_authenticated
+        )
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_staff:
+            return True
+        return obj.user == request.user
