fae7a66d448a8f8e02aad3bd4f31d3e6c9a2e221
Bohdan Yagginov <bhders62@gmail.com>
2025-09-21 22:50:38 +0300

    feature: added list and detail endpoints for payments model

--- config/urls.py
+++ config/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("api/library/", include("books.urls", namespace="books")),
     path("api/users/", include("users.urls", namespace="users")),
     path("api/borrowings/", include("borrowings.urls", namespace="borrowings")),
+    path("api/payments/", include("payments.urls", namespace="payments")),
 ]
