08cf02b0fb1daf2106271339726e387aad63f15f
Farzam <farzamgt@gmail.com>
2025-09-21 09:42:09 +0300

    feat(borrowings): implement full Borrowings API with business logic in model

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
-from .views import BorrowingCreateView, BorrowingDetailView, BorrowingListView
+from .views import (
+    BorrowingCreateView,
+    BorrowingDetailView,
+    BorrowingListView,
+    BorrowingReturnView,
+)
 
 app_name = "borrowings"
 
@@ -8,4 +13,9 @@ urlpatterns = [
     path("", BorrowingCreateView.as_view(), name="borrowings-create"),
     path("list/", BorrowingListView.as_view(), name="borrowings-list"),
     path("<int:pk>/", BorrowingDetailView.as_view(), name="borrowings-detail"),
+    path(
+        "<int:pk>/return/",
+        BorrowingReturnView.as_view(),
+        name="borrowings-return",
+    ),
 ]
