1d1e314631371b20ef9807c7e468845cb5fc93b3
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-22 15:13:08 +0300

    refactor: created new permission IsAdminOrReadOnly and added it to permission_classes

--- books/views.py
+++ books/views.py
@@ -1,9 +1,8 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
-from rest_framework.permissions import AllowAny
 
-from base.permissions import IsAdminOrIsAuthenticatedReadOnly
+from base.permissions import IsAdminOrReadOnly
 from books.models import Book
 from books.serializers import BookSerializer
 
@@ -11,7 +10,7 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminOrIsAuthenticatedReadOnly, ]
+    permission_classes = [IsAdminOrReadOnly, ]
     filter_backends = [
         DjangoFilterBackend,
         SearchFilter,
@@ -25,10 +24,3 @@ class BookViewSet(viewsets.ModelViewSet):
         "daily_fee",
         "inventory",
     )
-
-    def get_permissions(self):
-        if self.action in ["list", "retrieve"]:
-            permission_classes = [AllowAny, ]
-        else:
-            permission_classes = [IsAdminOrIsAuthenticatedReadOnly, ]
-        return [permission() for permission in permission_classes]
