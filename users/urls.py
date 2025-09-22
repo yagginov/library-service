b40303b5efe2e6f8dfda86f825306df8ce4fc579
yarik <super.racing@ukr.net>
2025-09-22 19:52:16 +0300

    created change password logic

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from users.views import UserProfileView, UserRegistrationView
+from users.views import (
+    UserProfileView,
+    UserRegistrationView,
+    ChangeUserPasswordView,
+)
 
 urlpatterns = [
     path("", UserRegistrationView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("me/", UserProfileView.as_view(), name="me"),
+    path("me/change-password/", ChangeUserPasswordView.as_view()),
 ]
 
 app_name="users"
