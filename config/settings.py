04e04c2fbf601be718945a6b7425d7d256fc0185
Otstoy <otstoy.sxe@gmail.com>
2025-09-22 11:01:11 +0300

    update: change jwt header to Authorize

--- config/settings.py
+++ config/settings.py
@@ -140,6 +140,7 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
+    "AUTH_HEADER_NAME": "HTTP_AUTHORIZE",
 }
 
 TG_API_TOKEN = os.environ["TG_API_TOKEN"]
