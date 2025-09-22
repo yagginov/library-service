c2e82f4c6a513a110d119e4092d572cb7fc26a78
yarik <super.racing@ukr.net>
2025-09-22 18:12:18 +0300

    changed user permissions

--- users/views.py
+++ users/views.py
@@ -1,17 +1,19 @@
 from rest_framework import generics
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 from users.serializers import UserProfileSerializer, UserRegistrationSerializer
 
 
 class UserRegistrationView(CreateAPIView):
     serializer_class = UserRegistrationSerializer
+    permission_classes = [AllowAny,]
 
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated,]
 
     def get_object(self):
         return self.request.user
