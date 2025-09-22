ddb1cfce58d3b1547f1ceb39b8ab6e8fc3120532
yarik <super.racing@ukr.net>
2025-09-22 20:01:58 +0300

    added path name

--- users/urls.py
+++ users/urls.py
@@ -1,22 +1,15 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-<<<<<<< HEAD
-from users.views import (
-    UserProfileView,
-    UserRegistrationView,
-    ChangeUserPasswordView,
-)
-=======
 from users.views import ChangeUserPasswordView, UserProfileView, UserRegistrationView
->>>>>>> 833943a (fixed with ruff)
+
 
 urlpatterns = [
     path("", UserRegistrationView.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
     path("me/", UserProfileView.as_view(), name="me"),
-    path("me/change-password/", ChangeUserPasswordView.as_view()),
+    path("me/change-password/", ChangeUserPasswordView.as_view(), name="change-password"),
 ]
 
 app_name="users"
