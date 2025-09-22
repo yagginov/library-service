a60dcbb4f771a8fab6f56a30cd3bd9410f82c27a
yarik <super.racing@ukr.net>
2025-09-22 18:36:13 +0300

    ruff check

--- users/views.py
+++ users/views.py
@@ -1,7 +1,6 @@
 from rest_framework import generics
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.schemas import user_profile_schema, user_registration_schema
 from users.serializers import UserProfileSerializer, UserRegistrationSerializer
