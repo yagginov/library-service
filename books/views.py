25ee3ab5f225cc1f6b179b57a2d8088656997040
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-22 10:48:18 +0300

    feature: changed permissions фll users can view the list and detailed information about books

--- books/views.py
+++ books/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.permissions import AllowAny
 
 from base.permissions import IsAdminOrIsAuthenticatedReadOnly
 from books.models import Book
@@ -10,7 +11,6 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [IsAdminOrIsAuthenticatedReadOnly, ]
     filter_backends = [
         DjangoFilterBackend,
         SearchFilter,
@@ -24,3 +24,10 @@ class BookViewSet(viewsets.ModelViewSet):
         "daily_fee",
         "inventory",
     )
+
+    def get_permissions(self):
+        if self.action in ["list", "retrieve"]:
+            permission_classes = [AllowAny, ]
+        else:
+            permission_classes = [IsAdminOrIsAuthenticatedReadOnly, ]
+        return [permission() for permission in permission_classes]
