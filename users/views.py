92e2aceb0cca29b429520d14e65ec0c43727479a
yarik <super.racing@ukr.net>
2025-09-22 19:52:16 +0300

    created change password logic

--- users/views.py
+++ users/views.py
@@ -1,17 +1,28 @@
 from rest_framework import generics
-from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
-from users.serializers import UserProfileSerializer, UserRegistrationSerializer
+from users.serializers import (
+    UserProfileSerializer,
+    UserRegistrationSerializer,
+    ChangeUserPassword
+)
 
 
-class UserRegistrationView(CreateAPIView):
+class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
     permission_classes = [AllowAny,]
 
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated,]
+
+    def get_object(self):
+        return self.request.user
+
+
+class ChangeUserPasswordView(generics.UpdateAPIView):
+    serializer_class = ChangeUserPassword
     permission_classes = [IsAuthenticated,]
 
     def get_object(self):
