6b2616fd8a5fdcfb96d0e999855934b6ea3d6ae4
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-09-23 11:58:36 +0300

    refactor: changed the name of validator

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -28,7 +28,7 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
         ]
         read_only_fields = ["id", "borrow_date", "actual_return_date"]
 
-    def validate(self, attrs):
+    def validate_deny(self, value):
         user = self.context["request"].user
 
         pending_payments = Payment.objects.filter(
@@ -40,7 +40,7 @@ class BorrowingCreateSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(
                 "You cannot borrow new books while you have pending payments."
             )
-        return attrs
+        return value
 
     def validate_book(self, value):
         if value.inventory <= 0:
