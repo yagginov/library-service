c462a883ef9fe6d79311d8ec99e92da9c4e622be
Otstoy <otstoy.sxe@gmail.com>
2025-09-23 13:39:11 +0300

    update(books): add id to list_display, add title adn author to list_editable

--- books/admin.py
+++ books/admin.py
@@ -5,7 +5,7 @@ from books.models import Book
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ["title", "author", "cover", "inventory", "daily_fee"]
-    list_editable = ["cover", "inventory", "daily_fee"]
+    list_display = ["id", "title", "author", "cover", "inventory", "daily_fee"]
+    list_editable = ["title", "author", "cover", "inventory", "daily_fee"]
     list_filter = ["author", "cover"]
     search_fields = ["title", "author"]
