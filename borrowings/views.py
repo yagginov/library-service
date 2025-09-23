2c869ce2364fdc8ef39688c1c6af5ca869157745
Bohdan Yagginov <bhders62@gmail.com>
2025-09-24 01:04:52 +0300

    feature: create new endpoint renew payment

--- borrowings/views.py
+++ borrowings/views.py
@@ -9,6 +9,9 @@ from borrowings.filters import BorrowingFilter
 from borrowings.models import Borrowing
 from borrowings.schemas import borrowings_viewset_schema
 from borrowings.serializers import BorrowingCreateSerializer, BorrowingDetailSerializer
+from payments.models import Payment
+from base.fine_service import fine_service
+from base.borrowing_service import borrowing_service
 
 
 @borrowings_viewset_schema
@@ -49,6 +52,41 @@ class BorrowingViewSet(
                            "Please complete the payment.",
             }
         return Response(response_data, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=["post"])
+    def renew_payment(self, request, pk=None):
+        borrowing = self.get_object()
+        payments = borrowing.payments.all()
+        responses = []
+
+        paid_payment = payments.filter(type=Payment.Type.PAYMENT, status=Payment.Status.PAID).first()
+        pending_payment = payments.filter(type=Payment.Type.PAYMENT, status=Payment.Status.PENDING).first()
+
+        if not paid_payment:
+            if pending_payment:
+                responses.append({"payment": "Active payment already exists"})
+            else:
+                payment = borrowing_service.renew_payment_for_borrowing(borrowing)
+                responses.append({"payment": f"New payment created with id {payment.id}"})
+
+        if borrowing.actual_return_date:
+            paid_fine = payments.filter(type=Payment.Type.FINE, status=Payment.Status.PAID).first()
+            pending_fine = payments.filter(type=Payment.Type.FINE, status=Payment.Status.PENDING).first()
+
+            if not paid_fine:
+                if pending_fine:
+                    responses.append({"fine": "Active fine payment already exists"})
+                else:
+                    fine = fine_service.create_fine_payment_if_overdue(borrowing)
+                    if fine:
+                        responses.append({"fine": f"New fine created with id {fine.id}"})
+                    else:
+                        responses.append({"fine": "You are not overdue your borrowing"})
+
+        if not responses:
+            return Response({"message": "Nothing to pay"}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(responses, status=status.HTTP_200_OK)
 
     def get_queryset(self):
         queryset = Borrowing.objects.select_related("book", "user")
