7a6e34df7199f4b26fb2ddeb7c95efb10c1b97cb
Bohdan Yagginov <bhders62@gmail.com>
2025-09-23 14:01:13 +0300

    fix: changed dto objects

--- base/dto.py
+++ base/dto.py
@@ -5,12 +5,12 @@ from pydantic import BaseModel, Field
 
 
 class ProductData(BaseModel):
-    name: str = Field(..., min_length=1, max_length=255)
-    description: str | None = None
+    name: str = Field(min_length=1, max_length=255, default="Product #1")
+    description: str = Field(default="Good choice")
 
 
 class PaymentSessionData(BaseModel):
-    product_data: ProductData = Field(default_factory=lambda: ProductData(name="Product"))
+    product_data: ProductData = Field(default_factory=lambda: ProductData())
     unit_amount: Decimal = Field(..., gt=Decimal("0.0"))
     quantity: int = Field(..., gt=0)
 
@@ -31,3 +31,5 @@ class PaymentData(BaseModel):
     status: PaymentStatus = Field(default=PaymentStatus.PENDING)
     type: PaymentType = Field(default=PaymentType.PAYMENT)
     fine_multiplier: Decimal = Field(gt=Decimal("0.0"), default=Decimal("1.0"))
+    product_data: ProductData = Field(default_factory=lambda: ProductData())
+    price: Decimal = Field(gt=Decimal("0.0"), default=Decimal("1.0"))
