b52dec8ec4d41d0718a0a03fbead0fd00efbff48
Otstoy <otstoy.sxe@gmail.com>
2025-09-23 10:26:10 +0300

    feat(notifications): add task which sends notification to admin chat about overdue borrowings update(docker-compose): add celery-beat volume update(settings): register celery-beat in apps refactor(services): add loop to send method

--- config/celery.py
+++ config/celery.py
@@ -6,4 +6,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 app = Celery("config")
 app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
+app.conf.update(
+    CELERY_IMPORTS=(
+        "notifications.tasks",
+    )
+)
+app.autodiscover_tasks(["notifications"])
---
+++ notifications/tasks.py
@@ -0,0 +1,22 @@
+from datetime import date
+
+from celery import shared_task
+
+import notifications.services
+from borrowings.models import Borrowing
+
+
+@shared_task
+def send_overdue_borrowings_messages():
+    today = date.today()
+    notification = notifications.services.notification
+    overdue_borrowings = Borrowing.objects.filter(
+        actual_return_date__isnull=True,
+        expected_return_date__lte=today
+    )
+
+    if overdue_borrowings:
+        for borrowing in overdue_borrowings:
+            notification.send(f"Book: {borrowing.book.title} borrowed {borrowing.borrow_date} is overdue")
+    else:
+        notification.send("No borrowings overdue today!")
