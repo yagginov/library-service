b8e866c4c71af0aeefab6c6a98c26b4d3d816bfd
Iryna Hromska <irahromska@gmail.com>
2025-09-24 08:17:50 +0300

    fix import sorting in signals.py

--- payments/signals.py
+++ payments/signals.py
@@ -1,8 +1,8 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-from payments.models import Payment
 from notifications.tasks import send_success_payment_notification
+from payments.models import Payment
 
 
 @receiver(post_save, sender=Payment)
