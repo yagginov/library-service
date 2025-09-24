faf3e3b71b2bc7c60fdad97e553278a73da15b96
Otstoy <otstoy.sxe@gmail.com>
2025-09-24 10:37:06 +0300

    fix: rework urls

--- borrowings/urls.py
+++ borrowings/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
+from rest_framework import routers
 
 from borrowings.views import BorrowingViewSet
 
 app_name = "borrowings"
 
-router = DefaultRouter()
-router.register("", BorrowingViewSet, basename="borrowings")
+router = routers.DefaultRouter()
+router.register("borrowings", BorrowingViewSet, basename="borrowings")
 
 urlpatterns = [
     path("", include(router.urls)),
